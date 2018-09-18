264beae488c6d5dea340a2779b8d21c2568bef08
embilombo <emreerendc@gmail.com>
2018-09-18 00:25:54 -0400

    friendfinder_1

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+var friends = [
+    {
+        name: "Zlatan Ibrahimovic",
+        photo: "http://39i0ntcz51a199kyr46emaf8.wpengine.netdna-cdn.com/wp-content/uploads/2018/09/ibrahimovic-1024x683.jpg",
+        answers: [
+            3,
+            4,
+            2,
+            5,
+            1,
+            3,
+            4,
+            2,
+            5,
+            3
+        ]
+    },
+    {
+        name: "Kate Upton",
+        photo: "https://pbs.twimg.com/profile_images/887828156886986752/F7XIdhSg_400x400.jpg",
+        answers: [
+            5,
+            4,
+            3,
+            2,
+            2,
+            3,
+            5,
+            4,
+            3,
+            4
+        ]
+    },
+    {
+        name: "Kevin Hart",
+        photo: "https://pbs.twimg.com/profile_images/839198522385793025/jPENEMJT_400x400.jpg",
+        answers: [
+            2,
+            2,
+            1,
+            4,
+            5,
+            5,
+            3,
+            1,
+            2,
+            2
+        ]
+    },
+    {
+        name: "Jeff Ross",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Jeff_Ross%2C_Joint_Base_Andrews%2C_May_2016_%28cropped%29.jpg",
+        answers: [
+            4,
+            4,
+            3,
+            2,
+            5,
+            4,
+            1,
+            3,
+            4,
+            3
+        ]
+    },
+    
+]
+
+module.exports = friends;
